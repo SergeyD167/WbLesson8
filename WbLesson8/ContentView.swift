c3bc0c02b9e7ccdf0208a8fce9bdd99fe99d8be6
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-07-09 23:47:26 +0300

    Initial Commit

---
+++ WbLesson8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WbLesson8
+//
+//  Created by Сергей Дятлов on 09.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WbLesson8/WbLesson8App.swift
@@ -0,0 +1,17 @@
+//
+//  WbLesson8App.swift
+//  WbLesson8
+//
+//  Created by Сергей Дятлов on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WbLesson8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
